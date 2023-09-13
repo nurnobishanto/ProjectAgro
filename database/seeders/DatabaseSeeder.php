29e04c8ec49ffa9e8573dbee191713b5a6e25819
Md Nurnobi Hosen <47949519+nurnobishanto@users.noreply.github.com>
2023-09-14 00:13:17 +0600

    Update 1.1

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\File;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,6 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        File::deleteDirectory(public_path('uploads'));
         $this->call(RoleSeeder::class);
         $this->call(AdminSeeder::class);
         // \App\Models\User::factory(10)->create();
