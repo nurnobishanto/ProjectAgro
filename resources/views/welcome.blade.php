1c7e44de292fb76d06733898cb2781b631ad3891
Md Nurnobi Hosen <47949519+nurnobishanto@users.noreply.github.com>
2023-09-14 00:37:52 +0600

    update pwa 1.0

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,7 +15,7 @@
 
     <!-- PWA  -->
     <meta name="theme-color" content="#6777ef"/>
-    <link rel="apple-touch-icon" href="{{ asset('logo.PNG') }}">
+    <link rel="apple-touch-icon" href="{{ asset('logo.png') }}">
     <link rel="manifest" href="{{ asset('/manifest.json') }}">
 </head>
 
