4f34e263ff61dda4c0b77132489c8dfa44158fc6
Md Nurnobi Hosen <47949519+nurnobishanto@users.noreply.github.com>
2023-10-21 15:49:35 +0600

    Expense
    add  Model and Migratton

---
+++ app/Models/ExpenseCategory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class ExpenseCategory extends Model
+{
+    use HasFactory, SoftDeletes;
+    protected $fillable = [
+        'name',
+        'status',
+        'created_by',
+        'updated_by',
+    ];
+
+    public function createdBy()
+    {
+        return $this->belongsTo(Admin::class, 'created_by');
+    }
+    public function updatedBy()
+    {
+        return $this->belongsTo(Admin::class, 'updated_by');
+    }
+}
---
+++ database/migrations/2023_10_19_070906_create_expense_categories_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('expense_categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->enum('status', ['active', 'deactivate'])->default('active');
+            $table->unsignedBigInteger('created_by');
+            $table->unsignedBigInteger('updated_by');
+            $table->timestamps();
+            $table->softDeletes();
+            $table->foreign('created_by')->references('id')->on('admins');
+            $table->foreign('updated_by')->references('id')->on('admins');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('expense_categories');
+    }
+};
