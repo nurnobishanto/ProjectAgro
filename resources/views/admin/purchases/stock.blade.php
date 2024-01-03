fd1d17cad15e457d5986386057b5d4967b409bca
Md Nurnobi Hosen <47949519+nurnobishanto@users.noreply.github.com>
2024-01-03 20:53:31 +0600

    Update stock.blade.php

--- resources/views/admin/purchases/stock.blade.php
+++ resources/views/admin/purchases/stock.blade.php
@@ -74,7 +74,7 @@
                                 <tr>
                                     <td>{{$sl++}}</td>
                                     <td>{{$stock->farm->name??'--'}}</td>
-                                    <td>{{$stock->product->name}}</td>
+                                    <td>{{$stock->product->name??'--'}}</td>
                                     <td>{{$stock->quantity}} <sup>{{$stock->product->unit->name}}</sup></td>
                                     <td>{{$stock->unit_price}} </td>
                                     <td>{{$stock->unit_price * $stock->quantity}}</td>
