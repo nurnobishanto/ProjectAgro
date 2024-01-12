7827ec4ab7f019657ac14fe6910735e06bcf64b9
Md Nurnobi Hosen <47949519+nurnobishanto@users.noreply.github.com>
2024-01-12 17:56:49 +0600

    Add Cattle Summery Report 1.3

--- app/Http/Controllers/Report/CattleReportController.php
+++ app/Http/Controllers/Report/CattleReportController.php
@@ -9,14 +9,14 @@ use Illuminate\Http\Request;
 class CattleReportController extends Controller
 {
     public function index(Request $request){
-        $cattles = Cattle::all();
+        $cattles = Cattle::where('status','active');
         //return $cattles;
         if ($request->farm_id){ $cattles = $cattles->where('farm_id',$request->farm_id); }
         if ($request->cattle_type_id){ $cattles = $cattles->where('cattle_type_id',$request->cattle_type_id); }
         if ($request->tag_id){ $cattles = $cattles->where('id',$request->tag_id); }
 
         $data = array();
-        $data['cattles'] = $cattles;
+        $data['cattles'] = $cattles->get();
         return view('report.cattle.index',$data);
     }
 }
