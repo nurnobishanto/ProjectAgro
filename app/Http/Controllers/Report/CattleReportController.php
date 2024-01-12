8732cdedc793d48ca506238c432145419debb594
Md Nurnobi Hosen <47949519+nurnobishanto@users.noreply.github.com>
2024-01-12 17:48:43 +0600

    Add Cattle Summery Report 1.0

--- app/Http/Controllers/Report/CattleReportController.php
+++ app/Http/Controllers/Report/CattleReportController.php
@@ -13,7 +13,7 @@ class CattleReportController extends Controller
         //return $cattles;
         if ($request->farm_id){ $cattles = $cattles->where('farm_id',$request->farm_id); }
         if ($request->cattle_type_id){ $cattles = $cattles->where('cattle_type_id',$request->cattle_type_id); }
-        if ($request->tag_id){ $cattles = $cattles->where('tag_id',$request->tag_id); }
+        if ($request->tag_id){ $cattles = $cattles->where('id',$request->tag_id); }
 
         $data = array();
         $data['cattles'] = $cattles;
