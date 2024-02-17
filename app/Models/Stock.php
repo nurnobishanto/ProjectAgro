4ceb8bba8d6af39e64fd1e4785fa17d1a791740e
Md Nurnobi Hosen <47949519+nurnobishanto@users.noreply.github.com>
2024-02-17 15:32:53 +0600

    wer

--- app/Models/Stock.php
+++ app/Models/Stock.php
@@ -17,7 +17,7 @@ class Stock extends Model
 
     public function product(): \Illuminate\Database\Eloquent\Relations\BelongsTo
     {
-        return $this->belongsTo(Product::class,'product_id');
+        return $this->belongsTo(Product::class,'product_id','id');
     }
     public function farm(): \Illuminate\Database\Eloquent\Relations\BelongsTo
     {
