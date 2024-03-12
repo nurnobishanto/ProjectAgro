85ad93ddd5e01acf4e55647f19337cf2f5926ce7
Md Nurnobi Hosen <47949519+nurnobishanto@users.noreply.github.com>
2024-03-12 11:23:47 +0600

    find bug 1.0

--- resources/views/admin/feedings/index.blade.php
+++ resources/views/admin/feedings/index.blade.php
@@ -20,151 +20,151 @@
 @stop
 
 @section('content')
-    <div class="row">
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-body">
-                    <form action="{{route('admin.feedings.create')}}" method="GET">
+{{--    <div class="row">--}}
+{{--        <div class="col-md-12">--}}
+{{--            <div class="card">--}}
+{{--                <div class="card-body">--}}
+{{--                    <form action="{{route('admin.feedings.create')}}" method="GET">--}}
 
-                        @if (count($errors) > 0)
-                            <div class = "alert alert-danger">
-                                <ul>
-                                    @foreach ($errors->all() as $error)
-                                        <li>{{ $error }}</li>
-                                    @endforeach
-                                </ul>
-                            </div>
-                        @endif
-                        <div class="row">
-                            <div class="col-lg-3 col-md-4 col-sm-6">
-                                <div class="form-group">
-                                    <label for="farm_id">{{__('global.select_farm')}} <span class="text-danger"> *</span></label>
-                                    <select class="select2 form-control" name="farm_id" id="farm_id">
-                                        <option value="">{{__('global.select_farm')}}</option>
+{{--                        @if (count($errors) > 0)--}}
+{{--                            <div class = "alert alert-danger">--}}
+{{--                                <ul>--}}
+{{--                                    @foreach ($errors->all() as $error)--}}
+{{--                                        <li>{{ $error }}</li>--}}
+{{--                                    @endforeach--}}
+{{--                                </ul>--}}
+{{--                            </div>--}}
+{{--                        @endif--}}
+{{--                        <div class="row">--}}
+{{--                            <div class="col-lg-3 col-md-4 col-sm-6">--}}
+{{--                                <div class="form-group">--}}
+{{--                                    <label for="farm_id">{{__('global.select_farm')}} <span class="text-danger"> *</span></label>--}}
+{{--                                    <select class="select2 form-control" name="farm_id" id="farm_id">--}}
+{{--                                        <option value="">{{__('global.select_farm')}}</option>--}}
 
-                                    </select>
-                                </div>
-                            </div>
-                            <div class="col-lg-3 col-md-4 col-sm-6">
-                                <div class="form-group">
-                                    <label for="cattle_type_id">{{ __('global.select_cattle_type')}}<span class="text-danger"> *</span></label>
-                                    <select name="cattle_type_id" id="cattle_type_id" class="form-control select2">
-                                        <option value="">{{ __('global.select_cattle_type')}}</option>
+{{--                                    </select>--}}
+{{--                                </div>--}}
+{{--                            </div>--}}
+{{--                            <div class="col-lg-3 col-md-4 col-sm-6">--}}
+{{--                                <div class="form-group">--}}
+{{--                                    <label for="cattle_type_id">{{ __('global.select_cattle_type')}}<span class="text-danger"> *</span></label>--}}
+{{--                                    <select name="cattle_type_id" id="cattle_type_id" class="form-control select2">--}}
+{{--                                        <option value="">{{ __('global.select_cattle_type')}}</option>--}}
 
-                                    </select>
-                                </div>
-                            </div>
-                            <div class="col-lg-3 col-md-4 col-sm-6">
-                                <div class="form-group">
-                                    <label for="feeding_group_id">{{ __('global.select_feeding_group')}}<span class="text-danger"> *</span></label>
-                                    <select name="feeding_group_id" id="feeding_group_id" class="form-control select2">
-                                        <option value="">{{ __('global.select_feeding_group')}}</option>
+{{--                                    </select>--}}
+{{--                                </div>--}}
+{{--                            </div>--}}
+{{--                            <div class="col-lg-3 col-md-4 col-sm-6">--}}
+{{--                                <div class="form-group">--}}
+{{--                                    <label for="feeding_group_id">{{ __('global.select_feeding_group')}}<span class="text-danger"> *</span></label>--}}
+{{--                                    <select name="feeding_group_id" id="feeding_group_id" class="form-control select2">--}}
+{{--                                        <option value="">{{ __('global.select_feeding_group')}}</option>--}}
 
-                                    </select>
-                                </div>
-                            </div>
-                            <div class="col-lg-3 col-md-4 col-sm-6 align-self-end">
-                                <div class="form-group">
-                                    @can('feeding_create')
-                                        <input type="submit" value="{{__('global.add_new')}}" class="btn btn-primary form-control">
-                                    @endcan
-                                </div>
-                            </div>
-                        </div>
+{{--                                    </select>--}}
+{{--                                </div>--}}
+{{--                            </div>--}}
+{{--                            <div class="col-lg-3 col-md-4 col-sm-6 align-self-end">--}}
+{{--                                <div class="form-group">--}}
+{{--                                    @can('feeding_create')--}}
+{{--                                        <input type="submit" value="{{__('global.add_new')}}" class="btn btn-primary form-control">--}}
+{{--                                    @endcan--}}
+{{--                                </div>--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
 
-                    </form>
-                </div>
-            </div>
-        </div>
-        <div class="col-12">
-            @can('feeding_list')
-                <div class="card">
-                    <div class="card-header">
-                        @can('feeding_delete')
-                            <a href="{{route('admin.feedings.trashed')}}" class="btn btn-danger mt-2">{{__('global.trash_list')}}</a>
-                        @endcan
-                    </div>
+{{--                    </form>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--        <div class="col-12">--}}
+{{--            @can('feeding_list')--}}
+{{--                <div class="card">--}}
+{{--                    <div class="card-header">--}}
+{{--                        @can('feeding_delete')--}}
+{{--                            <a href="{{route('admin.feedings.trashed')}}" class="btn btn-danger mt-2">{{__('global.trash_list')}}</a>--}}
+{{--                        @endcan--}}
+{{--                    </div>--}}
 
-                    <div class="card-body table-responsive">
-                        <table id="adminsList" class="table  dataTable table-bordered table-striped">
-                            <thead>
-                            <tr>
-                                <th>{{__('global.sl')}}</th>
-                                <th>{{__('global.date')}}</th>
-                                <th>{{__('global.farm')}}</th>
-                                <th>{{__('global.feeding_group')}}</th>
-                                <th>{{__('global.cattles')}}</th>
-                                <th>{{__('global.items')}}</th>
-                                <th>{{__('global.average')}}</th>
-                                <th>{{__('global.total')}}</th>
-                                <th>{{__('global.status')}}</th>
-                                <th>{{__('global.updated_at')}}</th>
-                                <th>{{__('global.action')}}</th>
-                            </thead>
-                            <tbody>
-                            <?php $sl = 1; ?>
-                            @foreach($feedings as $feeding)
-                                <tr>
+{{--                    <div class="card-body table-responsive">--}}
+{{--                        <table id="adminsList" class="table  dataTable table-bordered table-striped">--}}
+{{--                            <thead>--}}
+{{--                            <tr>--}}
+{{--                                <th>{{__('global.sl')}}</th>--}}
+{{--                                <th>{{__('global.date')}}</th>--}}
+{{--                                <th>{{__('global.farm')}}</th>--}}
+{{--                                <th>{{__('global.feeding_group')}}</th>--}}
+{{--                                <th>{{__('global.cattles')}}</th>--}}
+{{--                                <th>{{__('global.items')}}</th>--}}
+{{--                                <th>{{__('global.average')}}</th>--}}
+{{--                                <th>{{__('global.total')}}</th>--}}
+{{--                                <th>{{__('global.status')}}</th>--}}
+{{--                                <th>{{__('global.updated_at')}}</th>--}}
+{{--                                <th>{{__('global.action')}}</th>--}}
+{{--                            </thead>--}}
+{{--                            <tbody>--}}
+{{--                            <?php $sl = 1; ?>--}}
+{{--                            @foreach($feedings as $feeding)--}}
+{{--                                <tr>--}}
 
-                                    <td>{{$sl++}}</td>
-                                    <td>{{$feeding->date}}</td>
-                                    <td>{{$feeding->feedingGroup->farm->name??'--'}}</td>
-                                    <td>{{$feeding->feedingGroup->feeding_category->name??'--'}} {{$feeding->feedingGroup->feeding_moment->name??'--'}}</td>
-                                    <td>{{$feeding->cattles->count()??'--'}}</td>
-                                    <td>{{$feeding->products->count()??'--'}}</td>
-                                    <td>{{$feeding->per_cattle_cost??0}}</td>
-                                    <td>{{$feeding->total_cost??0}}</td>
-                                    <td>{{__('global.'.$feeding->status)}}</td>
-                                    <td>{{date_format($feeding->updated_at,'d M y h:i A') }}</td>
-                                    <td class="text-center">
-                                        @can('feeding_view')
-                                            <a href="{{route('admin.feedings.show',['feeding'=>$feeding->id])}}" class="btn btn-info px-1 py-0 btn-sm"><i class="fa fa-eye"></i></a>
-                                        @endcan
-                                        @if($feeding->status == 'pending')
-                                        <form action="{{ route('admin.feedings.destroy', $feeding->id) }}" method="POST">
-                                            @method('DELETE')
-                                            @csrf
+{{--                                    <td>{{$sl++}}</td>--}}
+{{--                                    <td>{{$feeding->date}}</td>--}}
+{{--                                    <td>{{$feeding->feedingGroup->farm->name??'--'}}</td>--}}
+{{--                                    <td>{{$feeding->feedingGroup->feeding_category->name??'--'}} {{$feeding->feedingGroup->feeding_moment->name??'--'}}</td>--}}
+{{--                                    <td>{{$feeding->cattles->count()??'--'}}</td>--}}
+{{--                                    <td>{{$feeding->products->count()??'--'}}</td>--}}
+{{--                                    <td>{{$feeding->per_cattle_cost??0}}</td>--}}
+{{--                                    <td>{{$feeding->total_cost??0}}</td>--}}
+{{--                                    <td>{{__('global.'.$feeding->status)}}</td>--}}
+{{--                                    <td>{{date_format($feeding->updated_at,'d M y h:i A') }}</td>--}}
+{{--                                    <td class="text-center">--}}
+{{--                                        @can('feeding_view')--}}
+{{--                                            <a href="{{route('admin.feedings.show',['feeding'=>$feeding->id])}}" class="btn btn-info px-1 py-0 btn-sm"><i class="fa fa-eye"></i></a>--}}
+{{--                                        @endcan--}}
+{{--                                        @if($feeding->status == 'pending')--}}
+{{--                                        <form action="{{ route('admin.feedings.destroy', $feeding->id) }}" method="POST">--}}
+{{--                                            @method('DELETE')--}}
+{{--                                            @csrf--}}
 
-                                            @can('feeding_update')
-                                                <a href="{{route('admin.feedings.edit',['feeding'=>$feeding->id])}}" class="btn btn-warning px-1 py-0 btn-sm"><i class="fa fa-pen"></i></a>
-                                            @endcan
-                                            @can('feeding_delete')
-                                                <button onclick="isDelete(this)" class="btn btn-danger btn-sm px-1 py-0"><i class="fa fa-trash"></i></button>
-                                            @endcan
-                                            @can('feeding_approve')
-                                                <a href="{{route('admin.feedings.approve',['feeding'=>$feeding->id])}}" class="btn btn-primary btn-sm px-1 py-0"><i class="fa fa-thumbs-up"></i></a>
-                                            @endcan
-                                        </form>
-                                        @else
-                                            <span class="btn btn-info px-1 py-0 btn-sm">Success</span>
-                                        @endif
-                                    </td>
-                                </tr>
-                            @endforeach
+{{--                                            @can('feeding_update')--}}
+{{--                                                <a href="{{route('admin.feedings.edit',['feeding'=>$feeding->id])}}" class="btn btn-warning px-1 py-0 btn-sm"><i class="fa fa-pen"></i></a>--}}
+{{--                                            @endcan--}}
+{{--                                            @can('feeding_delete')--}}
+{{--                                                <button onclick="isDelete(this)" class="btn btn-danger btn-sm px-1 py-0"><i class="fa fa-trash"></i></button>--}}
+{{--                                            @endcan--}}
+{{--                                            @can('feeding_approve')--}}
+{{--                                                <a href="{{route('admin.feedings.approve',['feeding'=>$feeding->id])}}" class="btn btn-primary btn-sm px-1 py-0"><i class="fa fa-thumbs-up"></i></a>--}}
+{{--                                            @endcan--}}
+{{--                                        </form>--}}
+{{--                                        @else--}}
+{{--                                            <span class="btn btn-info px-1 py-0 btn-sm">Success</span>--}}
+{{--                                        @endif--}}
+{{--                                    </td>--}}
+{{--                                </tr>--}}
+{{--                            @endforeach--}}
 
-                            </tbody>
-                            <tfoot>
-                            <tr>
-                                <th>{{__('global.sl')}}</th>
-                                <th>{{__('global.date')}}</th>
-                                <th>{{__('global.farm')}}</th>
-                                <th>{{__('global.feeding_group')}}</th>
-                                <th>{{__('global.cattles')}}</th>
-                                <th>{{__('global.items')}}</th>
-                                <th>{{__('global.average')}}</th>
-                                <th>{{__('global.total')}}</th>
-                                <th>{{__('global.status')}}</th>
-                                <th>{{__('global.updated_at')}}</th>
-                                <th>{{__('global.action')}}</th>
-                            </tr>
-                            </tfoot>
-                        </table>
-                    </div>
-                </div>
-            @endcan
+{{--                            </tbody>--}}
+{{--                            <tfoot>--}}
+{{--                            <tr>--}}
+{{--                                <th>{{__('global.sl')}}</th>--}}
+{{--                                <th>{{__('global.date')}}</th>--}}
+{{--                                <th>{{__('global.farm')}}</th>--}}
+{{--                                <th>{{__('global.feeding_group')}}</th>--}}
+{{--                                <th>{{__('global.cattles')}}</th>--}}
+{{--                                <th>{{__('global.items')}}</th>--}}
+{{--                                <th>{{__('global.average')}}</th>--}}
+{{--                                <th>{{__('global.total')}}</th>--}}
+{{--                                <th>{{__('global.status')}}</th>--}}
+{{--                                <th>{{__('global.updated_at')}}</th>--}}
+{{--                                <th>{{__('global.action')}}</th>--}}
+{{--                            </tr>--}}
+{{--                            </tfoot>--}}
+{{--                        </table>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            @endcan--}}
 
-        </div>
-    </div>
+{{--        </div>--}}
+{{--    </div>--}}
 
 @stop
 @section('footer')
